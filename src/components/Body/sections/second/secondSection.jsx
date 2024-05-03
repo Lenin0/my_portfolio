44e67699d974556988176c1bd16a76478ef2e489
Pedro lenin <pedrolenin06@gmail.com>
2024-05-03 10:04:39 -0300

    update in photos firts section

--- src/components/Body/sections/second/secondSection.jsx
+++ src/components/Body/sections/second/secondSection.jsx
@@ -1,10 +1,13 @@
-import styles from "./secondSection.module.css"
+import styles from "./secondSection.module.css";
 
 export const SecondSection = () => {
-    return (
-        <div className={styles.content}>
-            <p>Second</p>
-        </div>
-        
-    );
-}
+  return (
+    <div className={styles.content}>
+      <div className={styles.header}>
+      <div className={styles.line}></div>
+        <p>Some of my latest work</p>
+        <div className={styles.line}></div>
+      </div>
+    </div>
+  );
+};
